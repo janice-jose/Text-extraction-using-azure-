b720bc05de087c67296eabf92d47ac9fa2af09a1
RheaaDsouza <56199105+RheaaDsouza@users.noreply.github.com>
2022-03-28 21:41:49 +0530

    local image feature

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
-from flask import Flask, render_template, request, redirect,send_from_directory
+from flask import Flask, render_template, request, redirect,send_from_directory, url_for
 from azure.cognitiveservices.vision.computervision import ComputerVisionClient
-from azure.cognitiveservices.vision.computervision.models import OperationStatusCodes
 from azure.cognitiveservices.vision.computervision.models import VisualFeatureTypes
+from azure.cognitiveservices.vision.computervision.models import OperationStatusCodes
 from msrest.authentication import CognitiveServicesCredentials
 
-import time
-import requests
 import os
+import time
+from array import array
+from werkzeug.utils import secure_filename
 from dotenv import load_dotenv
 load_dotenv()
 
@@ -14,9 +15,9 @@ subscription_key = os.getenv('subscription_key')
 endpoint = os.getenv('endpoint')
 computervision_client = ComputerVisionClient(endpoint,CognitiveServicesCredentials(subscription_key))
 
-def extractTextFromImage(read):
-    #print(read)
-    #read_image = open(read, "rb")
+dirname = os.path.dirname(__file__)
+
+def read_local(read):
     # Call API with image and raw response (allows you to get the operation location)
     read_response = computervision_client.read_in_stream(read, raw=True)
     # Get the operation location (URL with ID as last appendage)
@@ -29,7 +30,6 @@ def extractTextFromImage(read):
         read_result = computervision_client.get_read_result(operation_id)
         if read_result.status.lower () not in ['notstarted', 'running']:
             break
-        
         time.sleep(5)
     l=[]
     if read_result.status == OperationStatusCodes.succeeded:
@@ -37,13 +37,45 @@ def extractTextFromImage(read):
         for text_result in read_result.analyze_result.read_results:
             for line in text_result.lines:
                 l.append(line.text)
-                #print(line.bounding_box)
     result=(' '.join(l))
     return result
-        
     
+       
+def extractTextFromImage(read_image_url):
+	read_response  = computervision_client.read(read_image_url ,  raw=True)
+	result = ''
+	# Get the operation location (URL with an ID at the end) from the response
+	read_operation_location = read_response.headers["Operation-Location"]
+	# Grab the ID from the URL
+	operation_id = read_operation_location.split("/")[-1]
+	
+	# Call the "GET" API and wait for it to retrieve the results 
+	while True:
+		read_result = computervision_client.get_read_result(operation_id)
+		if read_result.status not in ['notStarted', 'running']:
+			break
+		time.sleep(1)
+
+	# Add the detected text to result, line by line
+	if read_result.status == OperationStatusCodes.succeeded:
+		for text_result in read_result.analyze_result.read_results:
+			for line in text_result.lines:
+				result = result + " " + line.text
+				
+	return result
 
 app = Flask(__name__)
+
+
+UPLOAD_FOLDER = 'static/uploads/'
+app.secret_key = "secret key"
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
+
+ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
+
+def allowed_file(filename):
+	return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 # routes
 @app.route("/", methods=['GET', 'POST'])
@@ -53,18 +85,23 @@ def main():
 @app.route("/submit", methods = ['GET', 'POST'])
 def get_output():
     if request.method == 'POST':
-        file = request.files['i_remote']
-        #read image file string data
-    """npimg = numpy.fromstring(file, numpy.uint8)
-    # convert numpy array to image
-    img = cv2.imdecode(npimg, cv2.CV_LOAD_IMAGE_UNCHANGED)
-    #convert string data to numpy array"""
-    #print("file path is as follows",file)
-    result = extractTextFromImage(file)
-    return render_template("index.html", prediction = result)
-    #, img_path = image_url
+        image_url = request.form.get('image_url')
+    result = extractTextFromImage(image_url)
+    return render_template("index.html", prediction = result, img_path = image_url)
 
-
+@app.route("/upload-image", methods = ['GET', 'POST'])
+def upload_image():
+    if request.method == 'POST':
+        file = request.files['file']
+        
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            filepath=os.path.join(app.config['UPLOAD_FOLDER'],filename)
+            read_image= open(filepath, "rb")
+            result = read_local(read_image) 
+    return render_template("index.html" ,filename=filename,prediction=result)
+  
 if __name__ == '__main__':
     app.run(debug=True)   
 
